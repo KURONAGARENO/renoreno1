0ca3036b5905d02b82cb33466877c67f2b5d94d3
renoreno <renoreno0802@yahoo.co.jp>
2024-03-17 11:25:00 +0900

    背景プラグイン無効化

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,5 @@ var $plugins =
 [
 {"name":"MultiTilemap","status":true,"description":"複数タイルマップイベント作成プラグイン","parameters":{}},
 {"name":"NRP_ChangeCharacterSpeed","status":true,"description":"v1.01 キャラクターの移動速度を細かく変更します。","parameters":{"DefaultAlwaysDash":"","PlayerSpeed":"","PlusSpeedDash":"","<BasicSpeedRate>":"","PlayerBasicSpeedRate":"200","EventBasicSpeedRate":"100","<Vehicles>":"","BoatSpeed":"","ShipSpeed":"","AirShipSpeed":""}},
-{"name":"WindowBackImage","status":true,"description":"ウィンドウ背景画像指定プラグイン","parameters":{"windowImageInfo":""}}
+{"name":"WindowBackImage","status":false,"description":"ウィンドウ背景画像指定プラグイン","parameters":{"windowImageInfo":""}}
 ];
