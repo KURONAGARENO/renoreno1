eefff093cd8e80cb0b8bb3606299514fe2f3cf54
symsystem <symsystem.a10@gmail.com>
2024-03-28 10:01:02 +0900

    RemoveActorAll -> PartyActorRemoveAll

--- js/plugins/PartyActorRemoveAll.js
+++ js/plugins/PartyActorRemoveAll.js
@@ -2,12 +2,13 @@
 @target MZ
 @plugindesc パーティメンバー全員解除
 @author symsystem
-@url https://github.com/KURONAGARENO/renoreno1/blob/main/js/plugins/RemoveActorAll.js
+@url https://github.com/KURONAGARENO/renoreno1/blob/main/js/plugins/PartyActorRemoveAll.js
 
 @help
-「パーティメンバー全員解除」というプラグインコマンドを追加します
+「パーティメンバー全員解除」というプラグインコマンドを追加します。
+ただし、先頭メンバーは解除しません。
 
-@command RemoveActorAll
+@command PartyActorRemoveAll
 @text パーティメンバー全員解除
 @desc パーティメンバーを全員解除します
 */
@@ -17,7 +18,8 @@
 
   const _pluginName = document.currentScript.src.match(/^.*\/(.+)\.js$/)[1];
 
-  PluginManager.registerCommand(_pluginName, "RemoveActorAll", () => {
+  PluginManager.registerCommand(_pluginName, "PartyActorRemoveAll", () => {
+    // 先頭以外のIDを列挙して削除
     const actorIdsWithoutHead = $gameParty._actors.slice(1);
     for (const id of actorIdsWithoutHead) {
       $gameParty.removeActor(id);
